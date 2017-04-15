ba832926c6ac94ba18cfaf2961d501fb4ea8f969
Christopher Nugent <awedeven@gmail.com>
2017-04-15 12:04:01 -0700

    Added sentence generation capabilities.

--- MarkovGenerator.py
+++ MarkovGenerator.py
@@ -5,13 +5,23 @@ class MarkovGenerator:
 
     def __init__(self):
         self.letters = NGramMap()
+        self.words = NGramMap()
 
-    def new_input(self, new_input):
-        last = '\\start'
-        for char in new_input:
+    def new_word(self, word):
+        last = '\start'
+        for char in word:
             self.letters.add(last, char)
             last = char
         self.letters.add(last, '\end')
+
+    def new_sentence(self, sentence):
+        last = '\start'
+        words = sentence.split()
+        for word in words:
+            self.words.add(last, word)
+            last = word
+            self.new_word(word)
+        self.words.add(last, '\end')
 
     def next_word(self, max_count=-1):
         output = ''
@@ -19,4 +29,14 @@ class MarkovGenerator:
         while max_count != 0 and last != '\end':
             output += last
             last = self.letters.random_from(last)
+            max_count -= 1
         return output
+
+    def next_sentence(self, max_count=-1):
+        output = ''
+        last = self.words.random_from('\start')
+        while max_count != 0 and last != '\end':
+            output += last + ' '
+            last = self.words.random_from(last)
+            max_count -= 1
+        return output[:len(output)-1]
